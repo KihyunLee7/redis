26fd45bacd1cd73ab44c09a459eea97c4ae61552
이기현 <kihyun@wemakeprice.com>
2018-08-28 19:25:38 +0900

    .

--- src/main/java/com/wemakeprice/ad/redis/config/RedisConfig.java
+++ src/main/java/com/wemakeprice/ad/redis/config/RedisConfig.java
@@ -4,56 +4,72 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisSentinelPool;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @ComponentScan("com.wemakeprice.ad.redis")
 
 public class RedisConfig {
 
-    @Value("${redis.host}")
+    @Value("${spring.redis.host}")
     public String redisHost;
 
-    @Value("${redis.port}")
+    @Value("${spring.redis.port}")
     public int redisPort;
 
-    @Value("${redis.password}")
+    @Value("${spring.redis.password}")
     public String redisPassword;
+
+
+    @Value("${spring.redis.sentinel.nodes}")
+    public String[] sentinelNodes;
 
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-/*
         System.out.println("redisHost : " + redisHost + " / redisPort : " + redisPort + " / redisPassword : " + redisPassword);
 
 
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
         redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPassword));
-        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandaloneConfiguration);
-*/
 
 
-        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-                .master("mymaster")
-                .sentinel("127.0.0.1", 6386)
-                .sentinel("127.0.0.1", 6387)
-                .sentinel("127.0.0.1", 6388);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(100);
 
-        sentinelConfig.setPassword(RedisPassword.of(redisPassword));
 
-        JedisConnectionFactory factory = new JedisConnectionFactory(sentinelConfig);
-        return factory;
+        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+        jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60));// 60s connection timeout
+        jedisClientConfiguration.usePooling().poolConfig(jedisPoolConfig);
 
+
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration,
+                jedisClientConfiguration.build());
+
+
+        return  jedisConnectionFactory;
     }
+
+
+
+
+
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
@@ -66,6 +82,7 @@ public class RedisConfig {
 
         return template;
     }
+
 
 
 }
--- src/main/java/com/wemakeprice/ad/redis/repository/AdsRepository.java
+++ src/main/java/com/wemakeprice/ad/redis/repository/AdsRepository.java
@@ -3,10 +3,13 @@ package com.wemakeprice.ad.redis.repository;
 
 import com.wemakeprice.ad.redis.model.Ads;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 public interface AdsRepository {
     void save(Ads ads);
+
     Ads find(String keyword);
     void delete(String keyword);
     void deleteAll(String key);
